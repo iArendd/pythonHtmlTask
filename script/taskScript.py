a9dd5b099372fcba1b503ccd889b6671092e1014
Arend <bi_arend@hotmail.com>
2022-07-31 12:47:58 -0300

    Step 4
    Coleta do list e insere na lista de tasks criadas na tela.

--- script/taskScript.py
+++ script/taskScript.py
@@ -1,5 +1,18 @@
-
 from js import console
+from datetime import datetime
+
+tasks = []
+
+
+def atualiza_lista():
+
+    tasks_criadas = Element('tasks_criadas')
+    tasks_criadas.element.innerText = ""
+
+    for i in tasks:
+
+        tasks_criadas.element.innerText += f"{i['content']}\n"
+
 
 def cria_task(*args, **kags):
 
@@ -7,4 +20,41 @@ def cria_task(*args, **kags):
 
     task = input_task.element.value
 
-    console.log(task)
+    dict_task = {
+        
+                'task-id': len(task),
+                'content': task,
+                'data': datetime.now(),
+                'status': 'Criada'                    
+
+                }
+
+    tasks.append(dict_task)
+    atualiza_lista()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for i, j in dict_task.items():
+
+#       console.log(i, j)
+    
+        
+
